f04c0eaf927a20e035cd6009f9a0bcd571b84b0f
gamadv <lealdev.py@gmail.com>
2023-10-09 16:23:47 -0300

    feat Insert Data and Login rule to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { mockEventData } from '@/__mocks__/eventListData'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const hasLogged = localStorage.getItem('HASLOGGED')
+
+    if (hasLogged) {
+      router.push('/agenda')
+    } else {
+      localStorage.setItem('EVENTSDATA', JSON.stringify(mockEventData));
+      router.push('/signin')
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <main>
-      <p>Teste</p>
+    <main className='flex items-center w-full h-full justify-center'>
+      <strong className='text-3xl'>Seja Bem vindo 🍻</strong>{' '}
     </main>
   )
 }
