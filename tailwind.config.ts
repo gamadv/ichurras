2b140deb6533ba87689a871f1db6fff2f06f38b6
gamadv <lealdev.py@gmail.com>
2023-10-09 14:19:32 -0300

    feat: Add custom styles to TW config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {},
+      backgroundImage: {
+        iChurras_bg: 'linear-gradient(0deg, #FFD836 85%, rgba(255, 216, 54, 0.00) 100%)'
+      },
+      boxShadow: {
+        iChurras_cardShadow: '0px 0px 16px 0px rgba(0, 0, 0, 0.06)'
+      }
     },
   },
   plugins: [],
