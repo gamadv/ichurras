95803457a72b6c0c929e05adc3b9d2a0edfc2046
gamadv <lealdev.py@gmail.com>
2023-10-09 14:19:14 -0300

    feat: Create spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+export const Spinner = () => {
+  return (
+    <div className='w-auto h-auto flex justify-start items-center'>
+      <span
+        className='w-20 h-20 border-[#FFD836] rounded-full border-2 border-t-0 animate-spin'
+      />
+    </div>
+  )
+}
